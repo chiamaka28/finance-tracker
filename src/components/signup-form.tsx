9b46b3d9ca34e09d41c82f3cf244da2e166388cc
chiamaka28 <chiamakamaduike@gmail.com>
2026-01-20 13:23:43 +0100

    chore: add prettier, eslint, and husky for code quality

---
+++ src/components/signup-form.tsx
@@ -0,0 +1,21 @@
+import { Button } from '@/components/ui/button';
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSeparator,
+  FieldSet,
+} from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
+
+export default function SignupForm() {
+  return (
+    <form>
+      <FieldGroup>
+        <Field></Field>
+      </FieldGroup>
+    </form>
+  );
+}
